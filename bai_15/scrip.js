26e23ab62c27146822a875054bc04037b37ba220
Minh-123123 <10a2batbai2122004@gmail.com>
2025-11-03 18:20:56 +0700

    html bai15

---
+++ bai_15/scrip.js
@@ -0,0 +1,15 @@
+let danhSachPhim = [
+    {
+        id: 1;
+        tenPhim: "Mưa Đỏ";
+        namPhatHanh: 2025;
+        tuoi: 16;
+        thoiLuong: 2;
+        quocGia: "Việt Nam";
+        poster: ;
+        theLoai: "Phim Chiếu Rạp";
+    },
+    {
+
+    }
+]
